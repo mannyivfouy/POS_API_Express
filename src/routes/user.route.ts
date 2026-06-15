3b00cb730288d4ce02730c33974c57b3a16827cc
Mann Yivfouy <mannyivfouy@gmail.com>
2026-06-15 23:33:23 +0700

    add auth-middleware, authorize in user route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,48 @@
 import express from "express";
 import * as userController from "../controllers/user.controller";
 import { createUploader } from "../middlewares/upload.middleware";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { authorize } from "../middlewares/role.middleware";
 
 const router = express.Router();
 
 const uploadtemp = createUploader("temp");
 
-router.post("/create", uploadtemp.single("avatar"), userController.createUser);
-router.get("/", userController.getUsers);
-router.get("/:id", userController.getUserById);
-router.put("/update/:id", uploadtemp.single("avatar"),  userController.updateUser);
-router.delete("/:id", userController.deleteUser)
+router.post(
+  "/create",
+  authMiddleware,
+  authorize("admin"),
+  uploadtemp.single("avatar"),
+  userController.createUser,
+);
+
+router.get(
+  "/",
+  authMiddleware,
+  authorize("admin", "manager"),
+  userController.getUsers,
+);
+
+router.get(
+  "/:id",
+  authMiddleware,
+  authorize("admin", "manager"),
+  userController.getUserById,
+);
+
+router.put(
+  "/update/:id",
+  authMiddleware,
+  authorize("admin"),
+  uploadtemp.single("avatar"),
+  userController.updateUser,
+);
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  authorize("admin"),
+  userController.deleteUser,
+);
 
 export default router;
