4500090bbf7349d86e9ac29002cc8e3fe5e9e505
Mann Yivfouy <mannyivfouy@gmail.com>
2026-06-15 20:27:56 +0700

    stable version of User

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,8 +4,12 @@ import { createUploader } from "../middlewares/upload.middlewares";
 
 const router = express.Router();
 
-const uploadtemp = createUploader("temp")
+const uploadtemp = createUploader("temp");
 
 router.post("/create", uploadtemp.single("avatar"), userController.createUser);
+router.get("/", userController.getUsers);
+router.get("/:id", userController.getUserById);
+router.put("/update/:id", uploadtemp.single("avatar"),  userController.updateUser);
+router.delete("/:id", userController.deleteUser)
 
 export default router;
