cb47d1f62ecf7edde5eb5f29c2d32cad8525d268
Mann Yivfouy <mannyivfouy@gmail.com>
2026-06-17 17:40:25 +0700

    Fixed Role Ref With User

--- src/routes/role.route.ts
+++ src/routes/role.route.ts
@@ -1,8 +1,36 @@
 import express from "express";
 import * as roleController from "../controllers/role.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { authorize } from "../middlewares/role.middleware";
 
 const router = express.Router();
 
-router.post("/create", roleController.createRole);
+router.post(
+  "/create", 
+  authMiddleware,
+  authorize("Admin"),
+  roleController.createRole
+);
+
+router.get(
+  "/", 
+  authMiddleware,
+  authorize("Admin"),
+  roleController.getRoles
+);
+
+router.get(
+  "/:id", 
+  authMiddleware,
+  authorize("Admin"),
+  roleController.getRoleById
+);
+
+router.delete(
+  "/:id", 
+  authMiddleware,
+  authorize("Admin"),
+  roleController.deleteRole
+);
 
 export default router;
--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,10 +1,42 @@
 import Role from "../models/Role";
+import User from "../models/User";
 
 export const createRole = async (data: any) => {
-  const existingRole = await Role.findOne({name: data.name});
-  if(existingRole){
+  const existingRole = await Role.findOne({ name: data.name });
+  if (existingRole) {
     throw new Error("Role Already Exists");
   }
 
   return await Role.create(data);
-}
+};
+
+export const getRoles = async () => {
+  const role = await Role.find();
+  return role;
+};
+
+export const getRoleById = async (id: string) => {
+  const role = await Role.findById(id);
+
+  if (!role) {
+    throw new Error("Role Not Found");
+  }
+
+  return role;
+};
+
+export const deleteRole = async (id: string) => {
+  const role = await Role.findByIdAndDelete(id);
+
+  if (!role) {
+    throw new Error("Role Not Found");
+  }
+
+  const userCount = await User.countDocuments({ roleId: id });
+
+  if (userCount > 0) {
+    throw new Error("Cannot Delete Role Because It Is Used By User")
+  }
+
+  return { message: "Role Deleted Successfully" };
+};
